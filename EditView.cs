244380acb8ef8dd1d1610d088edc9191ba8d1d64
test <test@yahoo.co.jp>
2021-01-21 01:26:25 +0900

    たまにクリック位置が正しく反映されないことがあった

--- EditView.cs
+++ EditView.cs
@@ -522,7 +522,7 @@ namespace FooEditEngine
             p.Y -= this.render.TextArea.Y;
 
             //p.Y に最も近い行を調べる
-            var t = this.GetNearstRowAndOffsetY(this.Src.Row, p.Y);
+            var t = this.GetNearstRowAndOffsetY(this.Src.Row, p.Y - this.Src.OffsetY);
             double relX = 0, relY;
             if (t == null)
             {
